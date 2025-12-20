768de7456148bfcc26fd6b252b01ffa871290541
Trung Tính <vantrungtinh7@gmail.com>
2025-12-20 20:08:11 +0700

    Update My Order, lọc theo status, xoá create-details-edit-delete

--- FashionStore/Controllers/OrderController.cs
+++ FashionStore/Controllers/OrderController.cs
@@ -22,111 +22,32 @@ namespace FashionStore.Controllers
         }
 
         // GET: Order
-        public ActionResult Index()
+        public ActionResult Index(string status)
         {
-            var orders = db.Orders.Where(x => x.CustomerID == CustomerID).Include(o => o.Customer);
-            return View(orders.OrderByDescending(x=>x.OrderID).ToList());
+           
+            //var orders = db.Orders.Where(x => x.CustomerID == CustomerID).Include(o => o.Customer);
+            //return View(orders.OrderByDescending(x=>x.OrderID).ToList());
+
+            var orders = db.Orders
+              .Where(x => x.CustomerID == CustomerID);
+            //Lọc theo status
+            if (!string.IsNullOrEmpty(status))
+            {
+                orders = orders.Where(x => x.Status == status);
+            }
+            //Làm đầy đủ để tránh N + 1 query 
+            var result = orders
+               .Include(o => o.Customer)
+               .Include(o => o.OrderDetails.Select(od => od.ProductVariant))
+               .Include(o => o.OrderDetails.Select(od => od.ProductVariant.Product))
+               .Include(o => o.OrderDetails.Select(od => od.ProductVariant.Product.ProductImages))
+               .Include(o => o.OrderDetails.Select(od => od.ProductVariant.Color))
+               .Include(o => o.OrderDetails.Select(od => od.ProductVariant.Size))
+               .OrderByDescending(x => x.OrderID)
+               .ToList();
+            ViewBag.CurrentStatus = status;
+            return View(result);
         }
-
-        //// GET: Order/Details/5
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Order order = db.Orders.Find(id);
-        //    if (order == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(order);
-        //}
-
-        //// GET: Order/Create
-        //public ActionResult Create()
-        //{
-        //    ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "UserName");
-        //    return View();
-        //}
-
-        //// POST: Order/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "OrderID,CustomerID,OrderDate,Status,ShippingAddress,TotalAmount,PaymentMethod")] Order order)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Orders.Add(order);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "UserName", order.CustomerID);
-        //    return View(order);
-        //}
-
-        //// GET: Order/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Order order = db.Orders.Find(id);
-        //    if (order == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "UserName", order.CustomerID);
-        //    return View(order);
-        //}
-
-        //// POST: Order/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "OrderID,CustomerID,OrderDate,Status,ShippingAddress,TotalAmount,PaymentMethod")] Order order)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(order).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "UserName", order.CustomerID);
-        //    return View(order);
-        //}
-
-        //// GET: Order/Delete/5
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Order order = db.Orders.Find(id);
-        //    if (order == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(order);
-        //}
-
-        //// POST: Order/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    Order order = db.Orders.Find(id);
-        //    db.Orders.Remove(order);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
